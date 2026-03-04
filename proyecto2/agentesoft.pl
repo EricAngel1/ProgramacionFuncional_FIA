a15f8c8807ab860a07697c3147132dc1032777d9
Liz <lizeth.gutierrez1636@alumnos.udg.mx>
2026-03-04 14:13:55 -0600

    Subir archivos, con menu creado, nombres de participantes y listas creadas

---
+++ proyecto2/agentesoft.pl
@@ -0,0 +1,15 @@
+%Miembros del Equipo 7
+%Angel Alexis Serrano Hernández - 219906183
+%Eric Eduardo Angel Angel - 223992884
+%Carlos González Rodríguez - 220886978
+%Lizeth Gutiérrez Torres - 220616369
+%Cristhian German  Ramírez  Ruiz - 223992922
+
+menu:- nl,
+write("1. Buscar"),nl,
+write("2. Comprobar/Listar"),nl,
+write("3. Concatenar"),nl,
+write("4. Agregar"),nl, 
+write("5. Eliminar"),nl,          
+write("6. Longitud"),nl,
+write("7. Ordenamiento"),nl.           
---
+++ proyecto2/baseconocimiento.pl
@@ -0,0 +1,13 @@
+%Creación de listas conforme al tema de robot DaVinci
+%lista1
+robothw([torre_de_vision, consola_del_cirujano, carro_del_paciente]).
+%lista2
+robotsw([conectividad, funcionalidad_avanzada,gestion_de_datos,simulacion]).
+%lista3
+versiones([s,sp,x,xi]).
+%lista4
+evapreoperacion([historial_clinico, examenes_laboratorio,evaluacion_preanestesica, consentimiento_informados]).
+%lista5
+modulointeligente([evaluacion_preoperacion, frecuencia_cardiaca, hemorragia, infeccion, niveles_de_riesgo, presion_arterial, probabilidad_de_complicaciones, signos_vitales]).
+%lista6
+operacion([ginecologia, otorrinolaringologia,urologia,cirugia_general]).
