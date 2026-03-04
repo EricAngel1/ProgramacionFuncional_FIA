80814489888f06d809658aff32937e35a3f26916
AngelSH <angel.serrano9061@alumnos.udg.mx>
2026-03-03 18:52:17 -0600

    Menu y Funcion Pura

--- ProgramacionFuncional/programa.fsx
+++ ProgramacionFuncional/programa.fsx
@@ -1,1 +1,59 @@
 
+//Miembros del Equipo 7
+//Angel Alexis Serrano Hernandez - 219906183
+//Eric Eduardo Angel Angel - 223992884
+//Carlos González Rodríguez - 220886978
+//Lizeth Gutiérrez Torres - 220616369
+//Cristhian German  Ramírez  Ruiz - 223992922
+
+//Aqui ya estamos aplicando una de las caracteristicas de la programacion funcional
+// Por ejemplo en esta funcion, necesitamos poner "rec" para que pueda llamarse a si misma sin usar ningun bucle
+let rec menu() =
+
+    //En este caso se implemento un menu simple de las caracteristicas de la programacion funcional
+    printfn ""
+    printfn "############## Caracteristicas Pro. Funcional ##############"
+    printfn "1- Funcion Pura."
+    printfn "2- Inmutables."
+    printfn "3- Recursividad."
+    printfn "4- Salir."
+    printfn "#############################################"
+    printf "Escoge una opcion (1-4): " 
+    
+    let opcion = System.Console.ReadLine()
+
+    //creamos un switch pero con la sintaxis de F#
+    match opcion with
+    | "1" ->
+
+    // Aqui ya aplicamos la funcion pura
+        let sumar x y = x + y // siempre devuelve el mismo resultado con los mismas variables
+        let resultado = sumar 5 20
+        printfn "====================================="
+        printfn "La suma es: %d" resultado
+        printfn "====================================="
+
+        menu()
+    | "2" ->
+        printfn "====================================="
+        printfn "Prueba"
+        printfn "====================================="
+        menu()
+    | "3" ->
+        printfn "====================================="
+        printfn "Prueba2"
+        printfn "====================================="
+        menu()
+    | "4" ->
+        printfn "====================================="
+        printfn "Gracias por su atencion"
+        printfn "====================================="
+    | _ ->
+        printfn "====================================="
+        printfn "Escoge bien la opcion brother"
+        printfn "====================================="
+        menu()
+
+
+menu()
+
