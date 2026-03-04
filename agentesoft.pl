49d6c562d7d4e10df7ba0494839c5ee41d980537
Liz <lizeth.gutierrez1636@alumnos.udg.mx>
2026-03-04 13:40:23 -0600

    Añadir los miembros del equipo, y la creación del menú con sus opciones

--- agentesoft.pl
+++ agentesoft.pl
@@ -1,1 +1,16 @@
+%Miembros del Equipo 7
+%Angel Alexis Serrano Hernández - 219906183
+%Eric Eduardo Angel Angel - 223992884
+%Carlos González Rodríguez - 220886978
+%Lizeth Gutiérrez Torres - 220616369
+%Cristhian German  Ramírez  Ruiz - 223992922
 
+%creación del menú con las opciones
+menu:- nl,
+write("1. Buscar"),nl,
+write("2. Comprobar/Listar"),nl,
+write("3. Concatenar"),nl,
+write("4. Agregar"),nl, 
+write("5. Eliminar"),nl,          
+write("6. Longitud"),nl,
+write("7. Ordenamiento"),nl.
